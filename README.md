95daf693903bca9b7b51ae2fcc1520ed44d8576a
Dan-megabyte <theodledropbox@gmail.com>
2022-05-10 12:04:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-uciqa2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-uciqa2)
---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Range from './lib/Range.svelte'
+  import Exercises from './lib/Exercises.svelte'
+  Range.value = 3;
+</script>
+
+<main>
+  <h1>Ejercicios Algrebra</h1>
+
+  <label for="basic-range">Dificultad</label>
+  <Range on:change={(e) => Exercises.value = Range.value} />
+  <Exercises />
+  <p>
+    Encuentra ejercicios de todo tipos en esta pagina web!
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
